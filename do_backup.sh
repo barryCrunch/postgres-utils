e78e1ae5f9c4f00bc52cf0eb0aae22ab9c1242dc
Michael Barry <mbarry@packetdriving.com>
2020-12-29 16:16:25 -0600

    Fixed nested folder upload within blob

--- do_backup.sh
+++ do_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DUMP_FILE=/backup_`date +%Y%m%d_%H%M%S`.pgdump
+export DUMP_FILE=backup_`date +%Y%m%d_%H%M%S`.pgdump
 export PGPASSWORD=$POSTGRES_PASSWORD
 pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
 bzip2 $DUMP_FILE
